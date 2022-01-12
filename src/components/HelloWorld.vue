86caea918d7d3a1887c1faa22890a839a1d0083f
ZachHutsell <11369636+ZachHutsell@users.noreply.github.com>
2022-01-12 17:57:20 -0600

    Initial commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -6,35 +6,42 @@
       check out the
       <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
     </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <div class="container">
+      <div class="col"><input type="text" v-model="currentGuess" v-on:keyup.enter="submitGuess"></div>
+    </div>
+    <div class="container" v-for="guess in previousGuesses" :key="guess.word">
+      <!-- TODO should probably make following a component -->
+      <div v-for="letter in guess.word" class="item" :class="{perfect: isPerfect(letter)}" :key="letter">{{letter}}</div>
+    </div>
+  
   </div>
 </template>
 
 <script>
 export default {
   name: 'HelloWorld',
+  data() {
+    return {
+      solution: 'SURGE',
+      currentGuess: '',
+      previousGuesses: [{
+        word: 'MERGE',
+        correctLetters: 'RGE',
+      }]
+    }
+  },
   props: {
     msg: String
+  },
+  methods: {
+    submitGuess() {
+      this.currentGuess = this.currentGuess.toUpperCase();
+      this.previousGuesses.unshift({word: this.currentGuess, correctLetters: ''});//TODO
+      this.currentGuess = '';
+    },
+    isPerfect(letter) {
+      return this.solution.includes(letter);
+    }
   }
 }
 </script>
@@ -54,5 +61,25 @@ li {
 }
 a {
   color: #42b983;
+}
+.container {
+  display: flex;
+  justify-content: center;
+  gap: 15px;
+  align-items:center;
+}
+.item {
+  display: flex;
+  gap: 15px;
+  justify-content: center;
+}
+.col {
+  display: flex;
+  flex-direction: column;
+  gap: 15px;
+  justify-content: center;
+}
+.perfect {
+  background-color: goldenrod;
 }
 </style>
